c46b222ed5a5182a97434f668965f14b14ae1803
Vitor <vitusfranca@gmail.com>
2024-02-22 14:28:06 -0300

    inicializaçao do portfolio

---
+++ src/components/layout/banner/Banner.jsx
@@ -0,0 +1,12 @@
+import styles from './banner.module.css'
+
+function Banner() {
+    return (
+        <div className="conteiner-banner">
+            <h3>Banner da pagina</h3>
+        </div>
+
+    )
+}
+
+export default Banner
---
+++ src/components/layout/interface/Interface.jsx
@@ -0,0 +1,13 @@
+import Banner from '../banner/Banner.jsx' 
+
+function Interface () {
+
+    return (
+        <div>
+        <Banner/>
+        <h1>Inicio da pagina</h1>
+        </div>
+    )
+}
+
+export default Interface
