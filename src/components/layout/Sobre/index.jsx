db7294dc27159d94e9111f38261a90eadfb15ccf
Vitor <vitusfranca@gmail.com>
2024-03-19 09:40:33 -0300

    up

--- src/components/layout/Sobre/index.jsx
+++ src/components/layout/Sobre/index.jsx
@@ -1,11 +1,12 @@
 import styles from './styles.module.css'
+import img from '../../../assets/imgsobre.png'
 
 
 function Sobre( { scrollRef }) {
     return ( 
     <div ref={scrollRef} className={styles.conteiner}>
-        <div className={styles.conteiner_filho}><h3>Olá! Sou Vitor França, um entusiasta da tecnologia e desenvolvedor apaixonado. Minha jornada na programação começou em 2019 estudando em casa. Tenho experiência em Javascript. Meus valores incluem excelência técnica, colaboração e aprendizado contínuo. Estou animado com as oportunidades futuras e comprometido em criar um impacto positivo. Se quiser colaborar ou apenas bater um papo sobre tecnologia, estou à disposição!</h3></div>
-        <div className={styles.conteiner_filho}><h3>imagem aqui</h3></div>
+        <div className={styles.conteiner_texto}><h3>Olá! Sou Vitor França, um entusiasta da tecnologia e desenvolvedor com experiência em Javascript. <br/><br/> Valorizo a colaboração, busco aprendizado contínuo e estou comprometido em causar impacto positivo. <br/><br/> Aberto para colaborações e discussões sobre tecnologia.</h3></div>
+        <div className={styles.conteiner_img}><img src={img} alt="" /></div>
     </div>
 )
 }
