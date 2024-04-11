3de90296cb295585ef28c0602929559e6612ca88
Vitor <vitusfranca@gmail.com>
2024-04-11 12:57:47 -0300

    up4

--- src/components/layout/Sobre/index.jsx
+++ src/components/layout/Sobre/index.jsx
@@ -5,7 +5,17 @@ import img from '../../../assets/imgsobre.png'
 function Sobre( { scrollRef }) {
     return ( 
     <div ref={scrollRef} className={styles.conteiner}>
-        <div className={styles.conteiner_texto}><h3>Olá! Sou Vitor França, um entusiasta da tecnologia e desenvolvedor com experiência em FrontEnd. <br/><br/> Valorizo a colaboração, busco aprendizado contínuo e estou comprometido em causar impacto positivo. <br/><br/> Aberto para colaborações e discussões sobre tecnologia.</h3></div>
+        <div className={styles.conteiner_texto}>
+            <h1>Introdução Sobre Mim</h1>
+            <p>Olá! Sou Vitor França, um entusiasta da tecnologia 
+                <br/><br/>
+                desenvolvedor com experiência em FrontEnd. Fluente em javascript 
+                <br/><br/>
+                Valorizo a colaboração, busco aprendizado contínuo e estou comprometido em causar impacto positivo. 
+                <br/><br/> 
+                Aberto para colaborações e discussões sobre tecnologia.
+            </p>
+        </div>
         <div className={styles.conteiner_img}><img src={img} alt="" /></div>
     </div>
 )
