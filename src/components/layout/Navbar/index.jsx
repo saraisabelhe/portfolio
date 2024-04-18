81909f9f6a42a37680ce6bb9819b817f59e9ebb5
Vitor <vitusfranca@gmail.com>
2024-04-18 17:52:33 -0300

    up7

--- src/components/layout/Navbar/index.jsx
+++ src/components/layout/Navbar/index.jsx
@@ -1,11 +1,20 @@
-
+import React, { useState } from 'react';
+import { AiOutlineMenu } from 'react-icons/ai';
 import { Link } from 'react-router-dom'
 
 import styles from './styles.module.css'
 import Img from '../../../assets/logo5.png'
 
 
+
+
 function Navbar( {scrollComponenteB} ) {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
     
     return (
         <div className={styles.conteiner_header}>
@@ -13,6 +22,21 @@ function Navbar( {scrollComponenteB} ) {
                 <div className={styles.conteiner_logo}>
                     <img src={Img} alt="" />
                 </div>
+                <div className='Hamburguer-menu'>
+            <button className="hamburger-icon" onClick={toggleMenu}>
+                <AiOutlineMenu/>
+            </button>
+            {isOpen && (
+                <nav className="menu">
+                    <ul>
+                    <li><Link to="/">Home</Link></li>
+                        <li><Link to="/about">About</Link></li>
+                        <li><Link to="/contact">Contact</Link></li>
+                        <li><Link onClick={scrollComponenteB}>Sobre</Link></li>
+                    </ul>
+                </nav>
+            )}
+        </div>
                 <nav>
                     <ul>
                         <li><Link to="/">Home</Link></li>
