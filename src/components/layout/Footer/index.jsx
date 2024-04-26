295eb7c5f00be669aeb6f5c0229c3af3fca5eaa2
Vitor <vitusfranca@gmail.com>
2024-04-26 16:53:30 -0300

    perfil-acessibilidade

--- src/components/layout/Footer/index.jsx
+++ src/components/layout/Footer/index.jsx
@@ -7,10 +7,10 @@ function Footer( {email, insta, linkedin, github} ) {
         <div className={styles.conteiner}>
             <h2>&copy;2024 developer Vitor frança</h2>
             <div className={styles.conteiner_icon}>
-                <div className={styles.icon}><a href={linkedin}><FaLinkedin size={15}/></a></div>
-                <div className={styles.icon}><a href={github}><FaGithub size={15}/></a></div>
-                <div className={styles.icon}><a href={email}><FiMail size={15}/></a></div>
-                <div className={styles.icon}><a href={insta}><FaInstagram size={15}/></a></div>
+                <div className={styles.icon}><a href={linkedin} aria-label='Linkedin de Vitor França'><FaLinkedin size={15}/></a></div>
+                <div className={styles.icon}><a href={github} aria-label='Github de Vitor França'><FaGithub size={15}/></a></div>
+                <div className={styles.icon}><a href={email} aria-label='Enviar e-mail para Vitor França'><FiMail size={15}/></a></div>
+                <div className={styles.icon}><a href={insta} aria-label='Instagram de Vitor França'><FaInstagram size={15}/></a></div>
             </div>
         </div>
     )
